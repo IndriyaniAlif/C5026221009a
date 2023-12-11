5ba8247176d8da9daea92a4a1851e7383b7e18e3
Your Name <you@example.com>
2023-12-11 11:23:06 +0800

    latihan EAS new

--- resources/views/indexconter.blade.php
+++ resources/views/indexconter.blade.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-    <title>5026221179 - Kadek Mawar Kumala Dewi</title>
+    <title>5026221009 - Indriyani Alif Safitri</title>
 </head>
 
 <body>
